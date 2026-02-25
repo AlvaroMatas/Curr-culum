694d220dbc7a5d2d7f524ffa153b83c94241f3c9
alvmatada <alvmatada@alu.edu.gva.es>
2026-02-25 12:16:21 +0100

    6

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Animación de las barras de progreso al cargar la página
     const bars = document.querySelectorAll('.fill');
     
-    // Un pequeño retardo para que se vea el efecto de carga
     setTimeout(() => {
         bars.forEach(bar => {
-            // Obtenemos el ancho que pusimos en el HTML y lo aplicamos
             const targetWidth = bar.parentElement.previousElementSibling.querySelector('.pct').innerText;
-            // Si el texto es "Avanzado" ponemos 95%, si es "85%" lo limpiamos, etc.
             if(targetWidth.includes("Avanzado")) bar.style.width = "95%";
             else if(targetWidth.includes("85%")) bar.style.width = "85%";
             else if(targetWidth.includes("Medio-Bajo")) bar.style.width = "40%";
@@ -15,14 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }, 300);
 
-    // Añade esto dentro de tu DOMContentLoaded
 const projectCards = document.querySelectorAll('.project-card');
 
 projectCards.forEach(card => {
     card.addEventListener('click', (e) => {
         const url = card.getAttribute('href');
         console.log(`Navegando al proyecto: ${url}`);
-        // Aquí podrías añadir una animación de salida si quisieras
     });
 });
 });
